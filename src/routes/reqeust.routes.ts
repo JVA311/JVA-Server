794e953e37b7f5928c82ada35fefa24254116470
Kel-D-Coder <okolochibundu887@gmail.com>
2025-11-19 22:23:00 +0100

    tested oauth

--- src/routes/reqeust.routes.ts
+++ src/routes/reqeust.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth";
 import { upload } from "../middlewares/upload";
-import { createRequest, getAllRequests, getRequestById} from "../controllers/request.controller";
+import { createRequest, getAllRequests, getRequestById, getSingleRequest} from "../controllers/request.controller";
 import { searchRequests } from "../controllers/searchRequests.controller";
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.post("/create", authMiddleware, upload.array("documents"), createRequest)
 router.get("/", authMiddleware, getAllRequests);
 router.get("/getUserRequests", authMiddleware, getRequestById);
 router.get("/search", authMiddleware, searchRequests);
+router.get("/user/:id", authMiddleware, getSingleRequest);
 
 export default router;
